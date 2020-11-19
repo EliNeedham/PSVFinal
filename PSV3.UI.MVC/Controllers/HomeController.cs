ae6f31509615ff762c561d55b7186bc3cda82f26
EliNeedham <EliNeedham@outlook.com>
2020-11-19 13:41:46 -0600

    created new folder and project to be able to push to remote repo

---
+++ PSV3.UI.MVC/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+using System;
+using PSV3.UI.MVC.Models;
+
+
+
+namespace PSV3.UI.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        //[Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                string body = $"{cvm.Name} has sent you the following message: <br /> " +
+                    $"{cvm.Message} <strong>from the email address:</strong> {cvm.Email}";
+                MailMessage m = new MailMessage("no-reply@YouNeedham.net", "EliNeedham@outlook.com",
+                    cvm.Subject, body);
+
+                //allow html in email
+                m.IsBodyHtml = true;
+                m.Priority = MailPriority.High;
+                //m.CC.Add("elineedham@outlook.com");
+
+                //reply to the person who sent me an email
+                m.ReplyToList.Add(cvm.Email);
+
+                //configure mail client
+                SmtpClient client = new SmtpClient("mail.youneedham.net");
+                client.Credentials = new NetworkCredential("no-reply@youneedham.net", "Whatevs!2020");
+
+                try
+                {
+                    client.Send(m);
+                }
+                catch (Exception e)
+                {
+
+                    ViewBag.Message = e.StackTrace;
+                    return View(cvm);
+
+                }
+            }
+            return View("EmailConfirmation", cvm);
+        }
+
+        public ActionResult Portfolio()
+            {
+
+            return View();
+            }
+
+        public ActionResult Resume()
+        {
+
+            return View();
+        }
+
+        public ActionResult Links()
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ PSV3.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PSV3.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        //properties adn requirements
+        [Required(ErrorMessage = "* Your name is required")]
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)] //put validation with data attributes 
+        public string Email { get; set; }
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Message is required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
